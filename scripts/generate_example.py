61e7df6cb724aff2603710c1a68fb8c074dec09f
Ramon Melo <ramonmelo@ramonmelo.com>
2026-06-07 11:57:36 -0300

    Changed Generate Example from chat to generate API

--- scripts/generate_example.py
+++ scripts/generate_example.py
@@ -1,21 +1,21 @@
 # https://github.com/ollama/ollama-python
 
-from ollama import chat
-from ollama import ChatResponse
 import sys
+from ollama import generate
+from ollama import GenerateResponse
 
 def generate_example(text):
   prompt = f"""
   Please write one short, simple sentence using the target word provided at the end of this instruction. You must strictly use beginner-level vocabulary (A1 to A2 level) and very basic grammar. Output only the sentence itself, without any extra text, conversational filler, or explanations. The target word is: {text}"""
 
-  response: ChatResponse = chat(model='gemma4:latest', messages=[
-    {
-      'role': 'user',
-      'content': prompt,
-    }],
-    think=False)
+  response: GenerateResponse = generate(
+      model='gemma4:12b', 
+      prompt=prompt,
+      think=False,
+      stream=False
+  )
 
-  return response.message.content
+  return response.response
 
 if __name__ == "__main__":
   arguments = sys.argv[1:]
